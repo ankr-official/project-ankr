53acde7e6c297b47477dc5eba64a8b37d55e21ad
ankr-official <ankr.web.official@gmail.com>
2025-04-06 20:13:52 +0900

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,51 @@ import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <Router>
-            <div className="min-h-screen  rounded-lg">
+            <div className="rounded-lg">
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
+                <footer className="w-full bg-gray-900 text-gray-300 py-8 mt-8">
+                    <div className="container mx-auto px-4">
+                        <div className="flex flex-col items-center space-y-4">
+                            <div className="text-center">
+                                <p className="text-sm">
+                                    © {currentYear} ANKR. All rights reserved.
+                                </p>
+                            </div>
+                            <div className="text-sm flex divide-x divide-gray-700 [&>*]:px-2">
+                                <p>
+                                    <a
+                                        href="mailto:ankr.web.official@gmail.com"
+                                        className="text-indigo-400 hover:text-indigo-300"
+                                    >
+                                        Email
+                                    </a>
+                                </p>
+                                <p>
+                                    <a
+                                        href="https://x.com/ankr_db"
+                                        className="text-indigo-400 hover:text-indigo-300"
+                                    >
+                                        X (Twitter)
+                                    </a>
+                                </p>
+                                <p>
+                                    <a
+                                        href="https://www.instagram.com/ankr.db.official"
+                                        className="text-indigo-400 hover:text-indigo-300"
+                                    >
+                                        Instagram
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </Router>
     );
