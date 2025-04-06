3df96f3f57f85370ec4996076e6d6dc0929f52e9
ankr-official <ankr.web.official@gmail.com>
2025-04-06 20:56:53 +0900

    Update Vite base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
+    base: "/project-ankr/",
     plugins: [react()],
     css: {
         postcss: {
